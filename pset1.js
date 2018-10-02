fcb955ba652cd9864c986b4fdf970cea97c47b27
Mohammed Mosayed <mohammedmosayed@gmail.com>
2018-10-02 17:53:43 -0400

    Create pset1.js

---
+++ pset1.js
@@ -0,0 +1,24 @@
+/******************
+PROBLEM 1:
+
+1. Go to your Desktop and create a folder called 'my_portfolio'.
+2. Initialize Git in that folder.
+
+3. You should create following result within that repository:
+
+/my_portfolio
+    welcome.md
+    /resume
+        my_resume.doc
+    /cover_letter
+        my_cover_letter.doc
+       
+4. You must have the following branches and merged into master to create the result above:
+    - welcome
+    - resume
+    - cover_letter
+
+******************/
+  
+  
+  
